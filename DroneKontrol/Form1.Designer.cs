ae790ac67059f469ab1329168d404c8216559024
Aslıhan AKBULUT <56192462+aslihanakbulut@users.noreply.github.com>
2023-06-15 17:38:58 +0300

    Add files via upload

---
+++ DroneKontrol/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace DroneKontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mesafe = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbox_Port = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbox_brate = new System.Windows.Forms.ComboBox();
+            this.buton_baglan = new System.Windows.Forms.Button();
+            this.baglanti_durum = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.konum = new GMap.NET.WindowsForms.GMapControl();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(103, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(371, 31);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "DRONE KONTROL ARAYÜZÜ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.mesafe);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 354);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(251, 227);
+            this.groupBox2.TabIndex = 22;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "VERİLER";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Silver;
+            this.textBox4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox4.Location = new System.Drawing.Point(161, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(83, 25);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Silver;
+            this.textBox3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox3.Location = new System.Drawing.Point(67, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(83, 25);
+            this.textBox3.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(2, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "KONUM";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Silver;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox2.Location = new System.Drawing.Point(90, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 25);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "SICAKLIK";
+            // 
+            // mesafe
+            // 
+            this.mesafe.AutoSize = true;
+            this.mesafe.BackColor = System.Drawing.Color.Transparent;
+            this.mesafe.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mesafe.Location = new System.Drawing.Point(2, 42);
+            this.mesafe.Name = "mesafe";
+            this.mesafe.Size = new System.Drawing.Size(74, 17);
+            this.mesafe.TabIndex = 7;
+            this.mesafe.Text = "YÜKSEKLİK";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Silver;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(90, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 25);
+            this.textBox1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.groupBox1.Controls.Add(this.cbox_Port);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbox_brate);
+            this.groupBox1.Controls.Add(this.buton_baglan);
+            this.groupBox1.Controls.Add(this.baglanti_durum);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 95);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(251, 243);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BAĞLANTI";
+            // 
+            // cbox_Port
+            // 
+            this.cbox_Port.BackColor = System.Drawing.Color.Silver;
+            this.cbox_Port.FormattingEnabled = true;
+            this.cbox_Port.Location = new System.Drawing.Point(97, 34);
+            this.cbox_Port.Name = "cbox_Port";
+            this.cbox_Port.Size = new System.Drawing.Size(136, 25);
+            this.cbox_Port.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "PORT SEÇ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "BAUD RATE";
+            // 
+            // cbox_brate
+            // 
+            this.cbox_brate.BackColor = System.Drawing.Color.Silver;
+            this.cbox_brate.FormattingEnabled = true;
+            this.cbox_brate.Location = new System.Drawing.Point(97, 89);
+            this.cbox_brate.Name = "cbox_brate";
+            this.cbox_brate.Size = new System.Drawing.Size(136, 25);
+            this.cbox_brate.TabIndex = 19;
+            // 
+            // buton_baglan
+            // 
+            this.buton_baglan.BackColor = System.Drawing.Color.Gray;
+            this.buton_baglan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buton_baglan.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.buton_baglan.FlatAppearance.BorderSize = 0;
+            this.buton_baglan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buton_baglan.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buton_baglan.Location = new System.Drawing.Point(27, 174);
+            this.buton_baglan.Name = "buton_baglan";
+            this.buton_baglan.Size = new System.Drawing.Size(188, 60);
+            this.buton_baglan.TabIndex = 17;
+            this.buton_baglan.Text = "BAĞLAN";
+            this.buton_baglan.UseVisualStyleBackColor = false;
+            this.buton_baglan.Click += new System.EventHandler(this.buton_baglan_Click);
+            // 
+            // baglanti_durum
+            // 
+            this.baglanti_durum.AutoSize = true;
+            this.baglanti_durum.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.baglanti_durum.Location = new System.Drawing.Point(64, 141);
+            this.baglanti_durum.Name = "baglanti_durum";
+            this.baglanti_durum.Size = new System.Drawing.Size(133, 17);
+            this.baglanti_durum.TabIndex = 16;
+            this.baglanti_durum.Text = "BAĞLANTI DURUMU";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::DroneKontrol.Properties.Resources.KaqG;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(583, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // konum
+            // 
+            this.konum.Bearing = 0F;
+            this.konum.CanDragMap = true;
+            this.konum.EmptyTileColor = System.Drawing.Color.Navy;
+            this.konum.GrayScaleMode = false;
+            this.konum.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.konum.LevelsKeepInMemory = 5;
+            this.konum.Location = new System.Drawing.Point(280, 184);
+            this.konum.MarkersEnabled = true;
+            this.konum.MaxZoom = 2;
+            this.konum.MinZoom = 2;
+            this.konum.MouseWheelZoomEnabled = true;
+            this.konum.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.konum.Name = "konum";
+            this.konum.NegativeMode = false;
+            this.konum.PolygonsEnabled = true;
+            this.konum.RetryLoadTile = 0;
+            this.konum.RoutesEnabled = true;
+            this.konum.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.konum.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.konum.ShowTileGridLines = false;
+            this.konum.Size = new System.Drawing.Size(468, 397);
+            this.konum.TabIndex = 24;
+            this.konum.Zoom = 0D;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(784, 610);
+            this.Controls.Add(this.konum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Drone Kontrol Arayüzü";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label mesafe;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbox_Port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbox_brate;
+        private System.Windows.Forms.Button buton_baglan;
+        private System.Windows.Forms.Label baglanti_durum;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private GMap.NET.WindowsForms.GMapControl konum;
+    }
+}
+
---
+++ DroneKontrol/Form1.cs
@@ -0,0 +1,139 @@
+﻿using GMap.NET.MapProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+
+namespace DroneKontrol
+{
+
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort; // port nesnesi olustur
+        private string okunanVeri;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort = new SerialPort();
+            serialPort.DataReceived += SerialPort_DataReceived; // Veri alindiginda cagrilacak olay
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] port = SerialPort.GetPortNames();
+
+            foreach (string p in port)
+            {
+                cbox_Port.Items.Add(p);
+            }
+
+            if (port.Length > 0)
+            {
+                //   comboBox1.SelectedIndex = 0;    // form acildiginda secili olarak gelsin
+            }
+            cbox_brate.Items.Add("9600");
+            cbox_brate.Items.Add("38400");
+            cbox_brate.Items.Add("115200");
+
+
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+           // konum.DragButton == MouseButtons.Left;
+           //
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen == true)
+                serialPort.Close();
+
+           
+        }
+
+     
+
+        private void buton_baglan_Click(object sender, EventArgs e)
+        {
+
+            if (serialPort.IsOpen)
+            {
+                // baglanti aciksa kapat
+                serialPort.Close();
+                buton_baglan.Text = "BAĞLAN";
+                cbox_Port.Enabled = true;
+                cbox_brate.Enabled = true;
+                buton_baglan.BackColor = Color.Green;
+                baglanti_durum.Text = "BAĞLANTI YOK";
+
+                textBox1.Text = " ";
+                textBox2.Text = " ";
+                textBox3.Text = " ";
+                textBox4.Text = " ";
+
+            }
+            else
+            {
+                // secilen portu ac
+                string selectedPort = cbox_Port.SelectedItem.ToString();
+                int baudRate = int.Parse(cbox_brate.SelectedItem.ToString());
+                serialPort.PortName = selectedPort;
+                serialPort.BaudRate = baudRate;
+
+                try
+                {
+                    serialPort.Open();
+                    buton_baglan.Text = "BAĞLANTIYI KES ";
+                    cbox_Port.Enabled = false;
+                    cbox_brate.Enabled = false;
+                    buton_baglan.BackColor = Color.Red;
+                    baglanti_durum.Text = "BAĞLANDI";
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message);
+                }
+            }
+        }
+   
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //Veri alındığında bu olay tetiklenir
+            okunanVeri = serialPort.ReadLine();
+            this.Invoke(new EventHandler(displaydata));
+        }
+
+        private void displaydata(object sender, EventArgs e)
+        {
+             string[] GosterilecekDeger = okunanVeri.Split('/');
+            
+             textBox1.Text = GosterilecekDeger[0];
+             textBox2.Text = GosterilecekDeger[1];
+             textBox3.Text = GosterilecekDeger[2];
+             textBox4.Text = GosterilecekDeger[3];
+    
+
+            konum.MapProvider = GMapProviders.GoogleMap;
+            double enlem = Convert.ToDouble(textBox3.Text);
+            double boylam = Convert.ToDouble(textBox4.Text);
+            konum.Position = new GMap.NET.PointLatLng(enlem, boylam);
+            konum.MinZoom = 10;
+            konum.MaxZoom = 200;
+            konum.Zoom = 10;
+
+
+        }
+    }
+
+
+}
